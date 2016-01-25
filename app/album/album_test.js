8c0c84395229180d87b1e01f8a2fb6a005d4cf0f
Julen Landa Alustiza <julen@zokormazo.info>
2016-01-25 21:10:11 +0100

    fix album test

--- app/album/album_test.js
+++ app/album/album_test.js
@@ -1,15 +1,15 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('myApp.album module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('myApp.album'));
 
-  describe('view1 controller', function(){
+  describe('album controller', function(){
 
     it('should ....', inject(function($controller) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+      var albumController = $controller('albumController');
+      expect(albumController).toBeDefined();
     }));
 
   });
