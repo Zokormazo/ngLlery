6d9e59a94821ec56333ffbc4dc1814ccb18e0484
Julen Landa Alustiza <julen@zokormazo.info>
2016-11-02 12:41:16 +0100

    common: add actionBar component

--- app/components/common/common-components.js
+++ app/components/common/common-components.js
@@ -3,12 +3,33 @@
 angular.module('myApp.common')
 
 .component('alertBox', {
-  templateUrl : 'components/common/partials/alert-box.html',
+  templateUrl: 'components/common/partials/alert-box.html',
   controller: function(AlertService) {
     this.alerts = AlertService.getAlerts();
 
     this.closeAlert = function(index) {
       AlertService.deleteAlert(index);
+    }
+  }
+})
+
+.component('actionBar', {
+  bindings: {
+    searchModel: '=',
+    actions: '<'
+  },
+  templateUrl: 'components/common/partials/action-bar.html',
+  controller: function() {
+    if (angular.isDefined(this.searchModel)) {
+      this.search = true;
+    }
+    if (!angular.isDefined(this.actions)) {
+      this.actions = [];
+    }
+    this.isSearchCollapsed = true;
+
+    this.toggleSearch = function() {
+      this.isSearchCollapsed = !this.isSearchCollapsed;
     }
   }
 })
